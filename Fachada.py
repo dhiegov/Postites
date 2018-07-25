cd7fa309963c1603f58c5979b31fdffe569a2f42
DhiegoV <dhiegovinicius1@gmail.com>
2018-07-25 11:46:17 -0300

    adicionando arquivos

---
+++ Fachada.py
@@ -0,0 +1,20 @@
+
+class Fachada:
+  
+  def __init__():
+    pass
+
+  def criar_post-it():
+    pass
+
+  def criar_post-it_tarefa():
+    pass
+
+  def buscar_post-its():
+    pass
+  
+  def obter_dashboard():
+    pass
+  
+  def obter_post-it_arquivados():
+    pass
---
+++ Postit_tarefa.py
@@ -0,0 +1,12 @@
+from Postit import Postit
+
+class Postit_tarefa(Postit):
+
+  def __init__(tags, descricao, deadline):
+    super().__init__(tags, descricao)
+    self.deadline = deadline
+  
+  def __str__():
+    rtn = super().__str__()
+    rtn += ' (' + str(self.deadline) + ')'
+    return rtn
